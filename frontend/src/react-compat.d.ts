b05591decb296616c57efe5163608320b8ab96e5
Lucas Pecout <lucaspecout@gmail.com>
2026-08-05 17:08:58 +0200

    add

--- frontend/src/react-compat.d.ts
+++
@@ -1,6 +0,0 @@
-import 'react';
-
-declare module 'react' {
-  /** Compatibilité pour les références optionnelles initialisées au premier chargement API. */
-  function useRef<T = undefined>(): React.MutableRefObject<T | undefined>;
-}
